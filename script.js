fa3eb5030057509e8c594d1f3ad9c4826a83c373
Vincent <Vincentliu8823@gmail.com>
2025-07-02 23:12:27 -0400

    make 3x3 grid

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 console.log('hello from script');
 
-// Create a webpage with a 16x16 grid of square divs
-// try doing it in cosole log
+// Create a webpage with a 16x16 grid of square divs, make 3x3 grid
+let divContainer = document.querySelector('.divContainer');
+
+let rowContainer;
+let squareDiv;
+// for (let j=1; j<=3; j++)
+// {
+//     for (let i=1; i<=3; i++)
+//     {
+//         rowContainer = document.createElement('div');
+//         squareDiv = document.createElement('div');
+
+//         squareDiv.textContent = i;
+//         squareDiv.style.cssText = 'border: 2px solid black; '; //height: 20px; width: 20px
+
+        
+
+//         rowContainer.appendChild(squareDiv);
+//         divContainer.appendChild(rowContainer);
+//     }
+// }
+
+
+
